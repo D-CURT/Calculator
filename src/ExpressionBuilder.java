e89872db68503d2421c896d43dc52576d6ac4464
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-20 14:07:07 +0300

    Comparators added;

--- src/ExpressionBuilder.java
+++ src/ExpressionBuilder.java
@@ -5,14 +5,32 @@ public class ExpressionBuilder {
     public List<String> getRPN(String s) {
         ArrayList<String> list = new ArrayList<>();
         StringBuilder current = new StringBuilder();
+
         char character;
         for (int i = 0; i < s.length(); i++) {
             character = s.charAt(i);
             if (Character.isDigit(character)) {
-                current.append(character);
+                while (Character.isDigit(character))
+                    current.append(character);
+                list.add(current.toString());
             }
-            if (!current.equals("")) list.add(current.toString());
+            if (isOperator(String.valueOf(character))) {
+
+            }
         }
         return list;
+    }
+
+    public boolean isOperator(String s) {
+        switch (s) {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "^":
+            case "(":
+            case ")": return true;
+        }
+        return false;
     }
 }
---
+++ src/OperatorsComparable.java
@@ -0,0 +1,15 @@
+public class OperatorsComparable implements Comparable<String> {
+    @Override
+    public int compareTo(String o) {
+        switch (o) {
+            case "+": return 0;
+            case "-": return 1;
+            case "*": return 2;
+            case "/": return 3;
+            case "^": return 4;
+            case "(": return 5;
+            case ")": return 6;
+        }
+        return 7;
+    }
+}
