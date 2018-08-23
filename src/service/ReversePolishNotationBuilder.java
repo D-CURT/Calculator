944568337d68ba659f38502e0f8ede24d9242fe3
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-23 15:41:48 +0300

    Code corrections;

--- src/service/ReversePolishNotationBuilder.java
+++ src/service/ReversePolishNotationBuilder.java
@@ -5,8 +5,56 @@ import bean.Operator;
 import java.util.ArrayDeque;
 
 public class ReversePolishNotationBuilder {
+    private static final String SPACE = " ";
+
+    private void replaceOperand(char[] inputChars, StringBuilder resultString, int iterator) {
+        while (!isOperator(inputChars[iterator])) {
+            resultString.append(inputChars[iterator]).append(SPACE);
+            iterator++;
+            if (iterator == inputChars.length) break;
+        }
+        iterator--;
+    }
+
+    private void replaceOperator(char[] inputChars, ArrayDeque<Character> operators,
+                                 StringBuilder resultString, int iterator) {
+        Character operator;
+        if (inputChars[iterator] == Operator.LEFT_BRACKET.getSymbol()) {
+            operators.push(inputChars[iterator]);
+        } else if (inputChars[iterator] == Operator.RIGHT_BRACKET.getSymbol()) {
+            operator = operators.pop();
+            while (operator != Operator.LEFT_BRACKET.getSymbol()) {
+                resultString.append(operator).append(SPACE);
+                operator = operators.pop();
+            }
+        } else {
+            if (!operators.isEmpty()) {
+                while (Operator.comparePriority(inputChars[iterator], operators.peek()) < 1)
+                    resultString.append(operators.pop()).append(SPACE);
+            }
+            operators.push(inputChars[iterator]);
+        }
+    }
 
     public String getRPN(String input) {
+        ArrayDeque<Character> operators = new ArrayDeque<>();
+        char[] s = input.toCharArray();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < s.length; i++) {
+
+            if (Character.isDigit(s[i])) {
+                replaceOperand(s, result, i);
+            } else if (isOperator(s[i])) {
+                replaceOperator(s, operators, result, i);
+            }
+        }
+        while (!operators.isEmpty())
+            result.append(operators.pop()).append(operators.isEmpty() ? "" : SPACE);
+        return result.toString();
+    }
+
+    /*public String getRPN(String input) {
         ArrayDeque<Character> operators = new ArrayDeque<>();
         char[] s = input.toCharArray();
         String space = " ";
@@ -45,7 +93,7 @@ public class ReversePolishNotationBuilder {
         while (!operators.isEmpty())
             result.append(operators.pop()).append(operators.isEmpty() ? "" : space);
         return result.toString();
-    }
+    }*/
 
     public boolean isOperator(Character c) {
         return "()+-*/^".indexOf(c) != -1;
