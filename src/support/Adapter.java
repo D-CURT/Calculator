84c7a753b4758bd41b5e2846f49c5046fb7bba4e
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-26 23:07:58 +0300

    Code corrections;

--- src/support/Adapter.java
+++ src/support/Adapter.java
@@ -11,40 +11,43 @@ public class Adapter {
     }
 
     private static String adaptPow(String input) {
-        StringBuilder result = new StringBuilder();
-        StringBuilder current = new StringBuilder();
-        String num_after_brack;
-        int i_input_brack = 0;
-        char[] s = input.toCharArray();
-        ArrayDeque<String> operators = new ArrayDeque<>();
+        if (input.contains(POW)) {
+            StringBuilder result = new StringBuilder();
+            StringBuilder current = new StringBuilder();
+            String num_after_brack;
+            int i_input_brack = 0;
+            char[] s = input.toCharArray();
+            ArrayDeque<String> operators = new ArrayDeque<>();
 
-        for (int i = 0; i < s.length; i++) {
-            result.append(current.append(ELEMENT.readElement(s, i, ELEMENT.getType(s[i]))));
-            if (OPERATOR.found(current)) {
-                if (current.toString().equals(POW)) {
-                    result.append(LEFT_BRACKET);
-                    i_input_brack = i - current.length();
-                }
-                if (!operators.isEmpty() && !operators.peek().equals(current.toString())
-                        && operators.peek().equals(POW)) {
-                    while (!operators.isEmpty() && operators.peek().equals(POW)) {
-
-                        num_after_brack = ELEMENT.readElement(s, i_input_brack + 2, ELEMENT.getType(s[i_input_brack + 2]));
-                        int res_lng = result.length() - 1, i_brack = result.lastIndexOf(LEFT_BRACKET);
-
-                        if (i_brack == res_lng - num_after_brack.length() - current.length()) result.deleteCharAt(i_brack);
-                        else result.insert(result.length() - 1, RIGHT_BRACKET);
-                        operators.pop();
+            for (int i = 0; i < s.length; i++) {
+                result.append(current.append(ELEMENT.readElement(s, i, ELEMENT.getType(s[i]))));
+                if (OPERATOR.found(current)) {
+                    if (current.toString().equals(POW)) {
+                        result.append(LEFT_BRACKET);
+                        i_input_brack = i - current.length();
                     }
+                    if (!operators.isEmpty() && !operators.peek().equals(current.toString())
+                            && operators.peek().equals(POW)) {
+                        while (!operators.isEmpty() && operators.peek().equals(POW)) {
+
+                            num_after_brack = ELEMENT.readElement(s, i_input_brack + 2, ELEMENT.getType(s[i_input_brack + 2]));
+                            int res_lng = result.length() - 1, i_brack = result.lastIndexOf(LEFT_BRACKET);
+
+                            if (i_brack == res_lng - num_after_brack.length() - current.length()) result.deleteCharAt(i_brack);
+                            else result.insert(result.length() - 1, RIGHT_BRACKET);
+                            operators.pop();
+                        }
+                    }
+                    operators.push(current.toString());
                 }
-                operators.push(current.toString());
+                current.setLength(0);
             }
-            current.setLength(0);
+            while (!operators.isEmpty() && operators.peek().equals(POW)) {
+                result.append(RIGHT_BRACKET);
+                operators.pop();
+            }
+            return result.toString();
         }
-        while (!operators.isEmpty() && operators.peek().equals(POW)) {
-            result.append(")");
-            operators.pop();
-        }
-        return result.toString();
+        return input;
     }
 }
