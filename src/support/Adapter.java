aaa3831bb3b0dc61b5d786216c20c77b215654db
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-28 21:58:12 +0300

    in progress;

--- src/support/Adapter.java
+++ src/support/Adapter.java
@@ -3,6 +3,8 @@ package support;
 
 import beans.interfaces.FI_Adapter_onPosition;
 
+import java.util.ArrayDeque;
+
 import static support.constants.Constants.*;
 
 public class Adapter {
@@ -13,7 +15,7 @@ public class Adapter {
         return adaptPow(s.replaceAll(COMMA, POINT).replaceAll(SPACE, EMPTY));
     }
 
-    private static String adaptPow(String input) {
+    /*private static String adaptPow(String input) {
         StringBuilder result = new StringBuilder();
         if (input.contains(POW)) {
             char[] a = input.toCharArray();
@@ -45,16 +47,17 @@ public class Adapter {
                         if (!OPERATOR.isBracket(current)) {
                             if (!lb_r) {
                                 if (!current.toString().equals(POW)) {
-                                    //if (i != a.length - 1) {
-                                        //if (NUMBER.in(a[i + 1]) || LETTER.in(a[i + 1])) {
+                                    if (i != a.length - 1) {
+                                        if (!BRACKET.in(a[i + 1])) {
                                             tmp.append(RIGHT_BRACKET);
                                             lb_c--;
                                             rb = true;
-                                        //}
-                                   // }
-                                    if (rb && BRACKET.in(a[i + 1])) {
-                                        tmp.deleteCharAt(lb_i);
-                                        lb_c--;
+
+                                        }
+                                        if (!rb && BRACKET.in(a[i + 1])) {
+                                            tmp.deleteCharAt(lb_i);
+                                            lb_c--;
+                                        }
                                     }
                                     while (lb_c > 0) {
                                         tmp.append(RIGHT_BRACKET);
@@ -70,13 +73,17 @@ public class Adapter {
                                 lb_r = true; rb_r = false;
                             }
                             if (current.toString().equals(RIGHT_BRACKET)) {
+                                if (lb && lb_i == 1)  {
+                                    tmp.deleteCharAt(lb_i);
+                                    lb_c--;
+                                }
                                 rb_r = true; lb_r = false;
                             }
                         }
-                        if (!rb) {
+                        if (!rb && lb) {
                             tmp.append(current);
                         }
-                        rb = false;
+                        if (rb) rb = false;
                         current.setLength(0);
                     } else {
                         if (i == a.length - 1) {
@@ -91,6 +98,54 @@ public class Adapter {
                 } else {
                     result.append(current);
                     current.setLength(0);
+                }
+            }
+        }
+        return result.toString();
+    }*/
+
+    private static String adaptPow(String expression) {
+        char[] a = expression.toCharArray();
+        final int END = a.length - 1;
+        StringBuilder tmp = new StringBuilder();
+        String current;
+        StringBuilder result = new StringBuilder();
+        ArrayDeque<String> pows = new ArrayDeque<>();
+        boolean lb = false;
+
+        for (int i = 0; i < a.length; i++) {
+            current = ELEMENT.readElement(a, i, ELEMENT.getType(a[i]));
+            if (OPERATOR.found(current)) {
+                if (i != END) {
+                    if (current.equals(POW)) {
+                        if (NUMBER.in(a[i + 1]) || LETTER.in(a[i + 1])) {
+                            //tmp.append(current).append(LEFT_BRACKET);
+                            lb = true;
+                        }
+
+                        pows.push(current);
+                    }
+                }
+                result.append(lb ? current + LEFT_BRACKET : current);
+                //if (tmp.length() > 0) tmp.setLength(0);
+                if (pows.size() > 1) {
+                    while (!pows.isEmpty()) {
+                        result.append(RIGHT_BRACKET);
+                        pows.pop();
+                    }
+                    lb = false;
+                }
+            } else {
+                if (i != END && POW.equals(String.valueOf(a[i + 1]))) {
+                    /*if ()*/
+                }
+                result.append(current);
+                if (pows.size() > 1) {
+                    while (!pows.isEmpty()) {
+                        tmp.append(RIGHT_BRACKET);
+                        pows.pop();
+                    }
+                    lb = false;
                 }
             }
         }
