3f95624dac0ebd58e4de86bb76b96c57764d2df0
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-28 16:51:58 +0300

    AdaptPow in progress;

--- src/support/Adapter.java
+++ src/support/Adapter.java
@@ -8,6 +8,7 @@ import static support.constants.Constants.*;
 public class Adapter {
     public static final FI_Adapter_onPosition NUMBER = Character::isDigit;
     public static final FI_Adapter_onPosition LETTER = Character::isAlphabetic;
+    public static final FI_Adapter_onPosition BRACKET = (c) -> c == '(';
     public static String adapt(String s) {
         return adaptPow(s.replaceAll(COMMA, POINT).replaceAll(SPACE, EMPTY));
     }
@@ -19,8 +20,8 @@ public class Adapter {
             StringBuilder current = new StringBuilder();
             StringBuilder tmp = new StringBuilder();
 
-            boolean lb = false, rb = false;
-            int lb_i = 0, lb_c = 0;
+            boolean lb = false, rb = false, lb_r = false, rb_r = true;
+            int lb_i = 0, lb_c = 0, b_r_c = 0;
 
             for (int i = 0; i < a.length; i++) {
                 current.append(ELEMENT.readElement(a, i, ELEMENT.getType(a[i])));
@@ -31,34 +32,52 @@ public class Adapter {
                             lb_i = tmp.lastIndexOf(LEFT_BRACKET);
                             current.setLength(0);
                         }
-                        if (!lb) lb = true;
-                        lb_c++;
+                        if (!BRACKET.in(a[i + 1])) {
+                            if (!lb) lb = true;
+                            lb_c++;
+                        } else {
+                            tmp.append(current);
+                            current.setLength(0);
+                        }
                     }
                 } else if (lb) {
                     if (OPERATOR.found(current)) {
-                        if (!current.toString().equals(POW)) {
-                            if (i != a.length - 1) {
-                                if (NUMBER.in(a[i + 1]) || LETTER.in(a[i + 1])) {
-                                    tmp.append(RIGHT_BRACKET);
-                                    lb_c--;
-                                    rb = true;
+                        if (!OPERATOR.isBracket(current)) {
+                            if (!lb_r) {
+                                if (!current.toString().equals(POW)) {
+                                    //if (i != a.length - 1) {
+                                        //if (NUMBER.in(a[i + 1]) || LETTER.in(a[i + 1])) {
+                                            tmp.append(RIGHT_BRACKET);
+                                            lb_c--;
+                                            rb = true;
+                                        //}
+                                   // }
+                                    if (rb && BRACKET.in(a[i + 1])) {
+                                        tmp.deleteCharAt(lb_i);
+                                        lb_c--;
+                                    }
+                                    while (lb_c > 0) {
+                                        tmp.append(RIGHT_BRACKET);
+                                        lb_c--;
+                                    }
+                                    result.append(tmp).append(current);
+                                    tmp.setLength(0);
+                                    lb = false;
                                 }
                             }
+                        } else {
+                            if (current.toString().equals(LEFT_BRACKET)) {
+                                lb_r = true; rb_r = false;
+                            }
+                            if (current.toString().equals(RIGHT_BRACKET)) {
+                                rb_r = true; lb_r = false;
+                            }
                         }
-                        if (rb) {
-                            tmp.deleteCharAt(lb_i);
-                            lb_c--;
+                        if (!rb) {
+                            tmp.append(current);
                         }
-                        while (lb_c > 0) {
-                            tmp.append(RIGHT_BRACKET);
-                            lb_c--;
-                        }
-
-
-                        result.append(tmp).append(current);
-                        tmp.setLength(0);
+                        rb = false;
                         current.setLength(0);
-                        lb = false; rb = false;
                     } else {
                         if (i == a.length - 1) {
                             result.append(tmp.deleteCharAt(lb_i))
