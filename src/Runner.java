50ed4c9a45f9912de7a8bb5b1555b08802cb11dd
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-29 19:13:12 +0300

    Code corrections;

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,6 @@
 import services.ReversePolishNotationBuilder;
 import support.Adapter;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
-
 public class Runner {
     public static void main(String[] args) {
         //System.out.println(new ReversePolishNotationBuilder().getRPN("(8^3^(5+2)*5)/(1^3+(3*2-4^6)^2)"));
