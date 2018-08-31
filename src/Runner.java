ce83ec47d48d4defee9b71a723b35cd499de437c
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-31 22:01:28 +0300

    Code corrections;

--- src/Runner.java
+++ src/Runner.java
@@ -3,7 +3,7 @@ import support.Adapter;
 
 public class Runner {
     public static void main(String[] args) {
-        String s = "56-((84^3^(5+2^(2^3))*5)/(1^(2+3)+(3*2-4^6^2^(3+4)/78*(6^3^4))^2))";
+        String s = "56-((84^3^(5,2+2^(2^3))*5)/(1^(2+3)+(3*2-4^6^2^(3+4)/78*(6^3^4))^2))";
         String sa = Adapter.adapt(s);
         String sr = new ReversePolishNotationBuilder().toRPN(sa);
         System.out.println("Initial: " + s
