83b0953d8010b1a7ad9d3d4ee4f0719ecf6665a3
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-09-06 00:54:49 +0300

    Code corrections;

--- src/beans/elements/sub_elements/BracketsManager.java
+++ src/beans/elements/sub_elements/BracketsManager.java
@@ -3,10 +3,13 @@ package beans.elements.sub_elements;
 import beans.elements.Operator;
 import exceptions.CalculatorException;
 
+import static support.constants.Constants.LEFT_BRACKET;
+import static support.constants.Constants.RIGHT_BRACKET;
+
 public class BracketsManager {
     public boolean areBracketsAgreed(String s) {
         int check = 0;
-        if (s.contains("(") || s.contains(")")) {
+        if (s.contains(LEFT_BRACKET) || s.contains(RIGHT_BRACKET)) {
             for (int i = 0; i < s.length(); i++) {
                 check = s.charAt(i) == '(' ? check + 1 :
                         s.charAt(i) == ')' ? check - 1 : check;
