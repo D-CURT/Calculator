bc327594ccab03369cdaf8b4c646379339c1abef
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-09-06 22:02:30 +0300

    Code corrections;

--- src/controllers/InputController.java
+++ src/controllers/InputController.java
@@ -8,20 +8,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/input")
 public class InputController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        String exp = request.getParameter("result");
-        System.out.println(exp);
+        request.setAttribute("result",
+                new Calculator().calculate(request.getParameter("result")).getResult());
 
-        String exxxxx = new Calculator().calculate(exp).getResult();
-        System.out.println(exxxxx);
-        session.setAttribute("result", exxxxx);
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
         dispatcher.forward(request, response);
     }
